a23ba45d8dcce274efd7634f3c0efb80720709ac
Anatoliy Pologovoy <pologovoy89@mail.ru>
2023-03-02 14:43:58 +0300

    done task about map

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+type topCarsItemType = {
+  manufacturer : string,
+  model : string
+}
+
 function App() {
+  const topCars : topCarsItemType[] = [
+    {manufacturer:'BMW', model:'m5cs'},
+    {manufacturer:'Mercedes', model:'e63s'},
+    {manufacturer:'Audi', model:'rs6'}
+  ]
+
+  let rowsData = topCars.map((curr, i) => {
+      return (
+        <tr key={i+1}>
+          <td>{curr.manufacturer}</td>
+          <td>{curr.model}</td>
+        </tr>
+      )
+    })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <table>
+          <tbody>
+            {rowsData}
+          </tbody>
+        </table>
+        <ul>
+          {topCars.map((curr, i) => {
+            //debugger
+              return (
+                <li key={(i+1).toString()}>
+                  {curr.manufacturer}
+                  <span> {curr.model}</span>
+                </li>
+              )
+          })
+        }
+        </ul>
     </div>
   );
 }
