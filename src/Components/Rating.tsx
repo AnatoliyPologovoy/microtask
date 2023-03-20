ee34dd4e1a63e3f5bf8a68130fe2c008013364d1
AnatoliyPologovoy <pologovoy89@mail.ru>
2023-03-20 07:09:07 +0300

    add fill rating

--- src/Components/Rating.tsx
+++ src/Components/Rating.tsx
@@ -1,31 +1,57 @@
-import React, {useState} from 'react';
+import React, {MouseEvent, useState} from 'react';
+
+let styleLi = {
+    width: '5px',
+    height: '5px',
+    borderRadius: '50%',
+    border: '1px solid blue',
+    marginLeft: '2px',
+    cursor: 'pointer',
+}
+
+let styleUl = {
+    margin: '0',
+    padding: '0',
+    listStyle: 'none',
+    display: 'inline-flex',
+}
 
 const Rating = () => {
     let [rating, setRating] = useState(0)
 
-    let styleLi = {
-        width: '5px',
-        height: '5px',
-        borderRadius: '50%',
-        border: '1px solid blue',
-        marginLeft: '2px'
-    }
 
-    let styleUl = {
-        margin: '0',
-        padding: '0',
-        listStyle: 'none',
-        display: 'flex',
-        width: '100px'
+
+    const onMouseDownLiHandler = (e:MouseEvent<HTMLLIElement>) => {
+        setRating(parseFloat(e.currentTarget.id))
     }
 
     return (
         <ul style={styleUl}>
-            <li style={styleLi}></li>
-            <li style={styleLi}></li>
-            <li style={styleLi}></li>
+            <ItemRating isActive={rating > 0} id={'1'} onMouseDown={onMouseDownLiHandler} onMouseMove={onMouseDownLiHandler} />
+            <ItemRating isActive={rating > 1} id={'2'} onMouseDown={onMouseDownLiHandler} onMouseMove={onMouseDownLiHandler} />
+            <ItemRating isActive={rating > 2} id={'3'} onMouseDown={onMouseDownLiHandler} onMouseMove={onMouseDownLiHandler} />
         </ul>
     );
 };
+
+type ItemRatingPropsType = {
+    isActive: boolean
+    id: string
+    onMouseDown: (e:MouseEvent<HTMLLIElement>)=> void
+    onMouseMove: (e:MouseEvent<HTMLLIElement>)=> void
+}
+
+const ItemRating = (props:ItemRatingPropsType) => {
+    let currentStyle = props.isActive ? {...styleLi, backgroundColor: 'blue'} : styleLi
+
+    return (
+        <li style={currentStyle}
+            id={props.id}
+            onMouseMove={props.onMouseMove}
+            onMouseDown={props.onMouseDown}>
+
+        </li>
+    )
+}
 
 export default Rating;
