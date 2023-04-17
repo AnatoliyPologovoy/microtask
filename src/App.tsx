2890f741be6c1762538839a3be7746a604e7e9cc
AnatoliyPologovoy <pologovoy89@mail.ru>
2023-04-17 20:58:55 +0300

    create custom selector

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from './Todolist';
 import {v1} from 'uuid'
+import {Select} from "./components/Accordion";
 
 export type FilterValuesType = "all" | "active" | "completed";
 
@@ -22,99 +23,33 @@ type statTaskType = {
 }
 
 function App() {
-    // let [tasks, setTasks] = useState([
-    //     {id: v1(), title: "HTML&CSS", isDone: true},
-    //     {id: v1(), title: "JS", isDone: true},
-    //     {id: v1(), title: "ReactJS", isDone: false},
-    //     {id: v1(), title: "Rest API", isDone: false},
-    //     {id: v1(), title: "GraphQL", isDone: false},
-    // ]);
-    // let [filter, setFilter] = useState<FilterValuesType>("all");
-
-    let todolistID1 = v1();
-    let todolistID2 = v1();
-
-    let [todolists, setTodolists] = useState<Array<TodoListsType>>([
-        {id: todolistID1, title: 'What to learn', filter: 'all'},
-        {id: todolistID2, title: 'What to buy', filter: 'all'},
-    ])
-
-    let [tasks, setTasks] = useState<statTaskType>({
-        [todolistID1]: [
-            {id: v1(), title: "HTML&CSS", isDone: true},
-            {id: v1(), title: "JS", isDone: true},
-            {id: v1(), title: "ReactJS", isDone: false},
-            {id: v1(), title: "Rest API", isDone: false},
-            {id: v1(), title: "GraphQL", isDone: false},
-        ],
-        [todolistID2]: [
-            {id: v1(), title: "HTML&CSS2", isDone: true},
-            {id: v1(), title: "JS2", isDone: true},
-            {id: v1(), title: "ReactJS2", isDone: false},
-            {id: v1(), title: "Rest API2", isDone: false},
-            {id: v1(), title: "GraphQL2", isDone: false},
-        ]
-    });
-
-
-    function removeTask(taskId: string, todoListId: string) {
-        const updatedTasks = tasks[todoListId].filter(t => t.id !== taskId)
-        setTasks({...tasks, [todoListId]: updatedTasks})
-    }
-    function addTask(title: string, todoListId: string) {
-        let task = {id: v1(), title: title, isDone: false};
-        const updatedTasks = [task, ...tasks[todoListId]]
-        setTasks({...tasks, [todoListId]: updatedTasks})
-    }
-    function changeStatus(taskId: string, isDone: boolean, todoListId: string) {
-        // const updatedTasks = tasks[todoListId].map(t => {
-        //     return t.id === taskId ? {...t, isDone} : t
-        // })
-        // setTasks({...tasks, [todoListId]: updatedTasks})
-
-        const task = tasks[todoListId].find(t => t.id === taskId)
-        if (task) {
-            task.isDone = isDone
-            setTasks({...tasks, [todoListId]: [...tasks[todoListId]]})
+    const Cities = [
+        {
+            title: 'Moscow',
+            id: 1
+        },
+        {
+            title: 'London',
+            id: 2
+        },
+        {
+            title: 'Paris',
+            id: 3
+        },
+        {
+            title: 'Berlin',
+            id: 4
         }
-    }
-    function changeFilter(value: FilterValuesType, todoListId: string) {
-        setTodolists(todolists.map(tdl => {
-                return tdl.id === todoListId ? {...tdl, filter: value} : tdl
-            })
-        )
-    }
+    ]
 
-
-    const renderTodoLists = todolists.map(tdl => {
-        let tasksForTodolist = tasks[tdl.id];
-
-        if (tdl.filter === "active") {
-            tasksForTodolist = tasksForTodolist.filter(t => !t.isDone);
-        }
-        if (tdl.filter === "completed") {
-            tasksForTodolist = tasksForTodolist.filter(t => t.isDone);
-        }
-
-
-        return (
-            <Todolist
-                key={tdl.id}
-                title="What to learn"
-                id={tdl.id}
-                tasks={tasksForTodolist}
-                removeTask={removeTask}
-                changeFilter={changeFilter}
-                addTask={addTask}
-                changeTaskStatus={changeStatus}
-                filter={tdl.filter}
-            />
-        )
-    })
+    const [titleValue, setTitleValue] = useState<string>('Cities')
+    // const onChange = (title: string) => {
+    //     setTitleValue(title)
+    // }
 
     return (
         <div className="App">
-            {renderTodoLists}
+            <Select title={titleValue} body={Cities} onChange={setTitleValue}/>
         </div>
     );
 }
---
+++ src/components/Accordion.tsx
@@ -0,0 +1,125 @@
+import React, {KeyboardEvent, MouseEventHandler, useEffect, useState} from "react";
+import classes from "./Accordion.module.css"
+
+export type ItemsTypes = {
+    title: string,
+    id: number
+}
+
+type SelectPropsType = {
+    title: string,
+    body: ItemsTypes[],
+    onChange: (title: string) => void
+}
+
+export function Select(props: SelectPropsType): JSX.Element {
+    const [isCollapsed, SetIsCollapsed] = useState(true)
+
+
+    const onClickTitleHandler = () => {
+        SetIsCollapsed(!isCollapsed)
+    }
+
+    const chooseItem = (title: string) => {
+        props.onChange(title)
+        SetIsCollapsed(true)
+    }
+
+    const onBlurHandler = () => {
+        SetIsCollapsed(true)
+    }
+
+    const onPressUpHandler = (e: KeyboardEvent<HTMLDivElement>) => {
+        const items = props.body
+        if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
+            const currentItem = items.find(el=>el.title === props.title)
+            const isArrowDown = e.key === 'ArrowDown'
+            console.log(isArrowDown)
+            if (currentItem) {
+                const nextItemIndex = isArrowDown ? currentItem.id + 1 : currentItem.id - 1
+                const edgeItems = isArrowDown ? items.length - 1 : 0
+                const isNotLastItem = isArrowDown ?
+                    items.indexOf(currentItem) < edgeItems :
+                    items.indexOf(currentItem) > edgeItems
+                const newCurrentItem = isNotLastItem && items.find(el => el.id === nextItemIndex)
+                newCurrentItem && props.onChange(newCurrentItem.title)
+            }
+        }
+    }
+
+    const styleBody =  classes.body + ' ' + (isCollapsed ? classes.bodyHidden : '')
+
+    return (
+
+        <div className={classes.mainWrapper}
+             tabIndex={1}
+             onBlur={onBlurHandler}
+             onKeyUp={onPressUpHandler}
+        >
+            <SelectTitle title={props.title} onClick={onClickTitleHandler}/>
+            <SelectBody body={props.body}
+                        style={styleBody}
+                        chooseItem={chooseItem}
+                        currentTitle={props.title}
+            />
+        </div>
+    )
+}
+//-------------------------------
+
+type SelectTitlePropsType = {
+    title: string
+    onClick: () => void
+}
+
+function SelectTitle(props: SelectTitlePropsType): JSX.Element {
+    return (
+        <p onClick={props.onClick} className={classes.title}> {props.title}</p>
+    )
+}
+//------------------------
+
+
+type SelectBodyPropsType = {
+    body: ItemsTypes[]
+    style: string
+    chooseItem: (title: string) => void
+    currentTitle: string
+}
+
+function SelectBody(props: SelectBodyPropsType): JSX.Element {
+    const [currentTitle, setCurrentId] = useState<string>(props.currentTitle)
+
+    useEffect(() => {
+        setCurrentId(props.currentTitle)
+    }, [props.currentTitle])
+
+    const items = props.body.map(el => {
+        const choosingItem = () => {
+            props.chooseItem(el.title)
+            setCurrentId(el.title)
+        }
+
+        const isCurrentTitle = currentTitle === el.title
+        //isCurrentTitle && setCurrentId(el.id)
+
+        const styleItem = classes.item + ' ' + (isCurrentTitle ? classes.current : ' ')
+
+        return (
+            <li
+                key={el.id}
+                className={styleItem}
+                onClick={choosingItem}
+                onMouseEnter={() => setCurrentId(el.title)}
+            >
+                {el.title}
+            </li>
+        )
+    })
+
+    return (
+        <ul className={props.style}>
+            {items}
+        </ul>
+    )
+}
